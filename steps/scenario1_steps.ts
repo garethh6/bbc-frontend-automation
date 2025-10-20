1093edeeda5fe7d33aaf8964ae5813b026e49d8a
garethh6 <garethh6@gmail.com>
2025-10-20 23:20:01 +1300

    Update scenario1_steps.ts

--- steps/scenario1_steps.ts
+++ steps/scenario1_steps.ts
@@ -1,17 +1,20 @@
-import { Given, When, Then } from '@cucumber/cucumber';
+import { GivenWithTimeout, WhenWithTimeout, ThenWithTimeout } from '../support/step-timeout';
 import { expect } from '@playwright/test';
 import { BbcF1ResultsPage } from '../pages/BbcF1ResultsPage';
 
 const pageModel = new BbcF1ResultsPage();
 
-Given('I open the BBC Sport Formula 1 page', async () => {
+GivenWithTimeout('I open the BBC Sport Formula 1 page', async () => {
   await pageModel.openF1Page();
 });
 
-When('I navigate to the Las Vegas Grand Prix results', async () => {
+WhenWithTimeout('I navigate to the Las Vegas Grand Prix results', async () => {
   await pageModel.navigateToVegasResults();
 });
 
-Then('the results table should contain the following top 3 finishers:', async (dataTable) => {
-  await pageModel.verifyTop3(dataTable);
-});
+ThenWithTimeout(
+  'the results table should contain the following top 3 finishers:',
+  async (dataTable) => {
+    await pageModel.verifyTop3(dataTable);
+  }
+);
